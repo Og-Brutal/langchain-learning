12decb765fd905266092c075f0964f6bc110ef41
Abdul-Wahab <ogbrutal2825@gmail.com>
2026-08-10 18:23:28 +0500

    using temp paramter of invoke function.

--- Models/LLMs/LLM_Demo.py
+++ Models/LLMs/LLM_Demo.py
@@ -6,6 +6,6 @@ load_dotenv()  # Load environment variables from .env file
 
 llm = ChatGroq(model="llama-3.3-70b-versatile",)
 
-result = llm.invoke("Write a poem about the beauty of nature.")
+result = llm.invoke("Write a poem about the beauty of nature.",temperature=1.5)
 
 print("Poem : \n\n", result.content)
