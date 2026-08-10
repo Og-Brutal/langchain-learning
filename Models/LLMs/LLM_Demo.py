41cfb6eda65f897a0ef02343273c39816fd62397
Abdul-Wahab <ogbrutal2825@gmail.com>
2026-08-10 17:51:46 +0500

    docs: add LLM Demo example for LangChain models

---
+++ Models/LLMs/LLM_Demo.py
@@ -0,0 +1,11 @@
+
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
+llm = ChatGroq(model="llama-3.3-70b-versatile",)
+
+result = llm.invoke("Write a poem about the beauty of nature.")
+
+print("Poem : \n\n", result.content)
