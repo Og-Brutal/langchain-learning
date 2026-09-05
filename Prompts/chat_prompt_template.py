b9d5373d1b9d8bb1c884b646ee791d5a35878d67
Abdul-Wahab <ogbrutal2825@gmail.com>
2026-09-05 10:34:22 +0500

    Learned Chat Prompt Template to dynamically change the system and human messages.

---
+++ Prompts/chat_prompt_template.py
@@ -0,0 +1,20 @@
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env filec
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+chat_prompt = ChatPromptTemplate.from_messages([
+    ("system", "you are a helpful assistant that teaches {subject}"),
+    ("human","Tell me about {topic}"),
+])
+
+
+prompt=chat_prompt.invoke({"subject":"programming", "topic":"recursion"})
+
+llm=ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite", temperature=0.7)
+result=llm.invoke(prompt)
+
+
+
+print("Result : \n\n", result.content[0]['text'])
