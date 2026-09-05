7bd662a40a4a97b2a44aedd91cbff884f483531a
Abdul-Wahab <ogbrutal2825@gmail.com>
2026-09-05 10:53:06 +0500

    feat: update chat prompt template initialization and add message placeholder file

--- Prompts/chat_prompt_template.py
+++ Prompts/chat_prompt_template.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env filec
 
 from langchain_google_genai import ChatGoogleGenerativeAI
-chat_prompt = ChatPromptTemplate.from_messages([
+chat_prompt = ChatPromptTemplate([
     ("system", "you are a helpful assistant that teaches {subject}"),
     ("human","Tell me about {topic}"),
 ])
