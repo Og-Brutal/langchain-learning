be4288ca099b854c753a9b28b72bcf1f582b19a4
Abdul-Wahab <ogbrutal2825@gmail.com>
2026-09-05 09:46:58 +0500

    Temprature argument explained in comment.

--- Models/LLMs/LLM_Demo.py
+++ Models/LLMs/LLM_Demo.py
@@ -9,3 +9,7 @@ llm = ChatGroq(model="llama-3.3-70b-versatile",temperature=0.7)
 result = llm.invoke("Write a poem about the beauty of nature.")
 
 print("Poem : \n\n", result.content)
+
+
+
+# The Temperature argument controls the randomness of output like if you ask a question and in next prompt you ask the same question again it will give you different answer because of the randomness. The higher the temperature the more random the output will be and vice versa.
